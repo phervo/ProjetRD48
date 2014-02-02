c0e22dcf9a697dd0215eb537ff8cec887aaab0ab
paulfrancoisjeau <paulfrancois.jeau@gmail.com>
2014-02-02 14:45:59 +0100

    Correction Profondeur Champ

--- Sources/MagicPortrait/scripts_matlab/AugmentationProfondeurChamp.m
+++ Sources/MagicPortrait/scripts_matlab/AugmentationProfondeurChamp.m
@@ -25,13 +25,12 @@ function [imageRetour] = AugmentationProfondeurChamp(nomImage,nomImageRetour)
 
 	addpath('DefocusEstimation_Sources\');
 	
-    %Image Floue
-    imageOriginale=imread(nomImage);
-    w = fspecial('disk',16);
-    %w = fspecial('gaussian',[64 64],6);
-    imageFloue = imfilter(imageOriginale,w,'replicate');
-    figure(1);
-    imshow(imageFloue);
+%     %Image Floue
+%     imageOriginale=imread(nomImage);
+%     %w = fspecial('disk',16);
+%     w = fspecial('gaussian',[21 21],7);
+%     imageFloue = imfilter(imageOriginale,w,'replicate');
+    
     
     %Carte de profondeur
     imageOriginale_Double=im2double(imread(nomImage));
@@ -42,46 +41,62 @@ function [imageRetour] = AugmentationProfondeurChamp(nomImage,nomImageRetour)
     flouMaximum=3;
     [sDMap, fDmap] = defocusEstimation(imageOriginale_Double,carteBordure,std,lambda,flouMaximum);
 
-
-
-    %Detection de la boite englobante du visage
-    detecteurVisage = vision.CascadeObjectDetector; 		
-    boitesEnglobantes = step(detecteurVisage,imageOriginale); 
-    
-    x=boitesEnglobantes(1);
-    y=boitesEnglobantes(2);
-    hauteur_boite=boitesEnglobantes(3);
-    largeur_boite=boitesEnglobantes(4);
-    
-    %Calcul du seuil de profondeur moyen pour le visage dans la boite
-    SEUIL = mean(mean(fDmap(x:x+hauteur_boite,y:y+largeur_boite)));
-%    carteProfondeurFond = fDmap>SEUIL(1);
-    carteProfondeurFond = fDmap>1.25;
+    imshow(fDmap,[0 flouMaximum]);
 
     
-    imageRetour = zeros(size(imageOriginale),class(imageOriginale));
-    %Construction de l'image résultante, si à une position donnée, le pixel
-    %est à une profondeur supérieure à la profondeur moyenne au niveau du
-    %visage, on choisit la version floue sinon on prend la version
-    %originale du pixel
-    s = size(imageOriginale);
-    for i=1:s(1)
-        for j=1:s(2)
-            if(carteProfondeurFond(i,j)==1)
-                imageRetour(i,j,1) = imageFloue(i,j,1);
-                imageRetour(i,j,2) = imageFloue(i,j,2);
-                imageRetour(i,j,3) = imageFloue(i,j,3);
-            else
-                imageRetour(i,j,1) = imageOriginale(i,j,1);
-                imageRetour(i,j,2) = imageOriginale(i,j,2);
-                imageRetour(i,j,3) = imageOriginale(i,j,3);
-            end;
-        end;
-    end;
-    figure(3);
-    imshow(imageRetour);
     
-    %Ecriture du résultat
-    imwrite(imageRetour,nomImageRetour,'Quality',100); 
+    
+    
+    
+%     
+%     
+%     
+%     
+%     
+%     %Detection de la boite englobante du visage
+%     detecteurVisage = vision.CascadeObjectDetector; 		
+%     boitesEnglobantes = step(detecteurVisage,imageOriginale); 
+%     
+%     x=boitesEnglobantes(1);
+%     y=boitesEnglobantes(2);
+%     hauteur_boite=boitesEnglobantes(3);
+%     largeur_boite=boitesEnglobantes(4);
+%     
+%     figure(1);
+%     hold on;
+%     imshow(imageOriginale);
+%     rectangle('Position',[x,y,largeur_boite,hauteur_boite]);
+%     hold off;
+%     
+%     figure(2);
+%     imshow(imageFloue);
+%     
+%     %Calcul du seuil de profondeur moyen pour le visage dans la boite
+%     SEUIL = mean(mean(fDmap(x:x+hauteur_boite,y:y+largeur_boite)));
+%     carteProfondeurFond = fDmap>SEUIL(1)+0.2;
+%     %carteProfondeurFond = fDmap>1.25;
+% 
+%     
+%     imageRetour = zeros(size(imageOriginale),class(imageOriginale));
+%     %Construction de l'image résultante, si à une position donnée, le pixel
+%     %est à une profondeur supérieure à la profondeur moyenne au niveau du
+%     %visage, on choisit la version floue sinon on prend la version
+%     %originale du pixel
+%     s = size(imageOriginale);
+%     for i=1:s(1)
+%         for j=1:s(2)
+%             if(carteProfondeurFond(i,j)==1)
+%                 imageRetour(i,j,1:3) = imageOriginale(i,j,1:3)*0.5+imageFloue(i,j,1:3)*0.5;
+%             else
+%                 imageRetour(i,j,1:3) = imageOriginale(i,j,1:3);
+%             end;
+%         end;
+%     end;
+%     
+%     figure(3);
+%     imshow(imageRetour);
+%     
+%     %Ecriture du résultat
+%     imwrite(imageRetour,nomImageRetour,'Quality',100); 
 end
 
