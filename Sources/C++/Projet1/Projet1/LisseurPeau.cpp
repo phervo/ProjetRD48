0880b7d1020ed6da69d738aa0342a5bb18ea9ef7
paulfrancoisjeau <paulfrancois.jeau@gmail.com>
2014-02-18 10:20:41 +0100

    Commit Final

--- Sources/C++/Projet1/Projet1/LisseurPeau.cpp
+++ Sources/C++/Projet1/Projet1/LisseurPeau.cpp
@@ -17,11 +17,6 @@ Paul-François JEAU
 #include "opencv2\opencv.hpp"
 #include<stdio.h>
 
-/*Il existe 4 types de flous sous openCv, pour voir l'effet de chacun de ses flous, décommenter le code dans EssaiFlous et
-le lancer. Les 4 types de flous sont flou homogene, flou gaussien, flou médian, flou bilatéral.
-Nous avons choisi d'utiliser le flou gaussien car c'est celui qui montre les meilleurs résultats en ne détériorant pas trop l'image ou
-en donnant un aspet aquarel a la photo*/
-
 
 //-----Corps des méthodes/fonctions------------------------------------------
 //Constructeur
